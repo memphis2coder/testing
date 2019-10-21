d909a9886ef07ccbe233928a99abf325e8d6ef27
Antonio <Memphis2Coder@gmail.com>
2019-10-21 14:00:58 -0500

    Updates

---
+++ precache-manifest.3a086514d98bcb6ab405d574bc318f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c346834abd21c155459e721553d39b",
+    "url": "/memphis2coder/testing.git/index.html"
+  },
+  {
+    "revision": "d3fecfc047559761122e",
+    "url": "/memphis2coder/testing.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac5a9748a69dc3f61303",
+    "url": "/memphis2coder/testing.git/static/js/2.90147259.chunk.js"
+  },
+  {
+    "revision": "d3fecfc047559761122e",
+    "url": "/memphis2coder/testing.git/static/js/main.bb72a04f.chunk.js"
+  },
+  {
+    "revision": "0377268c76a21fe9ddd6",
+    "url": "/memphis2coder/testing.git/static/js/runtime-main.dac35e60.js"
+  }
+]);
